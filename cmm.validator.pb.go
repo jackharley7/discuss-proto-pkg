0923f7cd336b949982b0d77c8efb3d5f08f7dd80
Jack Harley <jack@3vilgenius.co>
2019-05-31 10:01:05 +0100

    update

--- cmm.validator.pb.go
+++ cmm.validator.pb.go
@@ -20,7 +20,7 @@ var _ = math.Inf
 func (this *User) Validate() error {
 	return nil
 }
-func (this *Link) Validate() error {
+func (this *CMMLink) Validate() error {
 	return nil
 }
 func (this *CMM) Validate() error {
