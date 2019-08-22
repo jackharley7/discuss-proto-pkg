8422772313ab45057b3d60391989937b8c722acb
Jack Harley <jack@3vilgenius.co>
2019-08-22 10:36:23 +0100

    make hashtag url available

--- hashtag.pb.gw.go
+++ hashtag.pb.gw.go
@@ -29,7 +29,7 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 
 var (
-	filter_HashTagService_GetHashTag_0 = &utilities.DoubleArray{Encoding: map[string]int{"name": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+	filter_HashTagService_GetHashTag_0 = &utilities.DoubleArray{Encoding: map[string]int{"category": 0, "name": 1}, Base: []int{1, 1, 2, 0, 0}, Check: []int{0, 1, 1, 2, 3}}
 )
 
 func request_HashTagService_GetHashTag_0(ctx context.Context, marshaler runtime.Marshaler, client HashTagServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
@@ -42,6 +42,17 @@ func request_HashTagService_GetHashTag_0(ctx context.Context, marshaler runtime.
 		err error
 		_   = err
 	)
+
+	val, ok = pathParams["category"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "category")
+	}
+
+	protoReq.Category, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "category", err)
+	}
 
 	val, ok = pathParams["name"]
 	if !ok {
@@ -128,7 +139,7 @@ func RegisterHashTagServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 }
 
 var (
-	pattern_HashTagService_GetHashTag_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"api", "hashtag", "name", "conversations"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_HashTagService_GetHashTag_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"api", "p", "category", "hashtag", "name"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
