ced1c3ea3d1ce7f2297bd69e47a729784c716fc3
Jack Harley <jack@3vilgenius.co>
2019-08-23 10:07:38 +0100

    cmm

--- cmm.pb.gw.go
+++ cmm.pb.gw.go
@@ -127,33 +127,6 @@ func request_CMMService_GetCMMsByMe_0(ctx context.Context, marshaler runtime.Mar
 
 }
 
-func request_CMMService_GetCMMByID_0(ctx context.Context, marshaler runtime.Marshaler, client CMMServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq GetCMMByIDRequest
-	var metadata runtime.ServerMetadata
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["cmmId"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "cmmId")
-	}
-
-	protoReq.CmmId, err = runtime.Int64(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "cmmId", err)
-	}
-
-	msg, err := client.GetCMMByID(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
 var (
 	filter_CMMService_SearchCMMs_0 = &utilities.DoubleArray{Encoding: map[string]int{"query": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
@@ -228,6 +201,33 @@ func request_CMMService_GetCMMsByIDs_0(ctx context.Context, marshaler runtime.Ma
 	}
 
 	msg, err := client.GetCMMsByIDs(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func request_CMMService_GetCMMByID_0(ctx context.Context, marshaler runtime.Marshaler, client CMMServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetCMMByIDRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["cmmId"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "cmmId")
+	}
+
+	protoReq.CmmId, err = runtime.Int64(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "cmmId", err)
+	}
+
+	msg, err := client.GetCMMByID(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
@@ -388,26 +388,6 @@ func RegisterCMMServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
-	mux.Handle("GET", pattern_CMMService_GetCMMByID_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_CMMService_GetCMMByID_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_CMMService_GetCMMByID_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("GET", pattern_CMMService_SearchCMMs_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -468,6 +448,26 @@ func RegisterCMMServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("GET", pattern_CMMService_GetCMMByID_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_CMMService_GetCMMByID_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CMMService_GetCMMByID_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_CMMService_GetCMMsByUserID_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -500,13 +500,13 @@ var (
 
 	pattern_CMMService_GetCMMsByMe_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "cmm", "me"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_CMMService_GetCMMByID_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "p", "cmm", "cmmId"}, "", runtime.AssumeColonVerbOpt(true)))
-
 	pattern_CMMService_SearchCMMs_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"api", "p", "cmm", "search", "query"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_CMMService_GetCMMOrder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "p", "cmm", "ids"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_CMMService_GetCMMsByIDs_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "p", "cmm"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_CMMService_GetCMMByID_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "p", "cmm", "cmmId"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_CMMService_GetCMMsByUserID_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"api", "p", "user", "userId", "cmm"}, "", runtime.AssumeColonVerbOpt(true)))
 )
@@ -520,13 +520,13 @@ var (
 
 	forward_CMMService_GetCMMsByMe_0 = runtime.ForwardResponseMessage
 
-	forward_CMMService_GetCMMByID_0 = runtime.ForwardResponseMessage
-
 	forward_CMMService_SearchCMMs_0 = runtime.ForwardResponseMessage
 
 	forward_CMMService_GetCMMOrder_0 = runtime.ForwardResponseMessage
 
 	forward_CMMService_GetCMMsByIDs_0 = runtime.ForwardResponseMessage
+
+	forward_CMMService_GetCMMByID_0 = runtime.ForwardResponseMessage
 
 	forward_CMMService_GetCMMsByUserID_0 = runtime.ForwardResponseMessage
 )
