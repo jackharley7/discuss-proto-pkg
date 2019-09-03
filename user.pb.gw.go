fa666121a98df0f599078e498bd6e216b2f8085d
Jack Harley <jack@3vilgenius.co>
2019-09-03 16:24:59 +0100

    update

--- user.pb.gw.go
+++ user.pb.gw.go
@@ -593,9 +593,20 @@ func local_request_UserService_GetTwitterUserByScreenName_0(ctx context.Context,
 
 }
 
+var (
+	filter_UserService_GetProfileImageUploadURL_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_UserService_GetProfileImageUploadURL_0(ctx context.Context, marshaler runtime.Marshaler, client UserServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetProfileImageUploadURLRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_UserService_GetProfileImageUploadURL_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.GetProfileImageUploadURL(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -605,6 +616,10 @@ func request_UserService_GetProfileImageUploadURL_0(ctx context.Context, marshal
 func local_request_UserService_GetProfileImageUploadURL_0(ctx context.Context, marshaler runtime.Marshaler, server UserServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetProfileImageUploadURLRequest
 	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_UserService_GetProfileImageUploadURL_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.GetProfileImageUploadURL(ctx, &protoReq)
 	return msg, metadata, err
